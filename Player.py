c84fbbaa2496cc20cc019418cf10357a692dcef9
ViniciusKrau <viniciuskrau@gmail.com>
2024-11-05 18:45:26 -0300

    segundo commit

--- Player.py
+++ Player.py
@@ -7,11 +7,12 @@ class Player:
 
     _name: str
     _score: int = field(default=0)
-    _opponentMatch1: float = field(default=0)
-    _opponentMatch2: float = field(default=0)
-    _opponentMatch3: float = field(default=0)
-    _isDrop: bool = field(default=False)
-    _isBye: bool = field(default=False)
+    _rounds_won: float = field(default=0)
+    _opponent_match2: float = field(default=0)
+    _opponent_match3: float = field(default=0)
+    _is_drop: bool = field(default=False)
+    _is_bye: bool = field(default=False)
+    _is_was_bye: bool = field(default=False)
 
     @property
     def name(self) -> str:
@@ -30,41 +31,49 @@ class Player:
         self._score = value
     
     @property
-    def opponentMatch1(self) -> float:
-        return self._opponentMatch1
+    def roundsWon(self) -> float:
+        return self._rounds_won
     
-    @opponentMatch1.setter
-    def opponentMatch1(self, value) -> None:
-        self._opponentMatch1 = value
+    @roundsWon.setter
+    def roundsWon(self, value) -> None:
+        self._rounds_won = value
 
     @property
     def opponentMatch2(self) -> float:
-        return self._opponentMatch2
+        return self._opponent_match2
     
     @opponentMatch2.setter
     def opponentMatch2(self, value) -> None:
-        self._opponentMatch2 = value
+        self._opponent_match2 = value
 
     @property
     def opponentMatch3(self) -> float:
-        return self._opponentMatch3
+        return self._opponent_match3
     
     @opponentMatch3.setter
     def opponentMatch3(self, value) -> None:
-        self._opponentMatch3 = value
+        self._opponent_match3 = value
 
     @property
     def isDrop(self) -> bool:
-        return self._isDrop
+        return self._is_drop
     
     @isDrop.setter
     def isDrop(self, value) -> None:
-        self._isDrop = value
+        self._is_drop = value
 
     @property
     def isBye(self) -> bool:
-        return self._isBye
+        return self._is_bye
     
     @isBye.setter
     def isBye(self, value) -> None:
-        self._isBye = value
+        self._is_bye = value
+
+    @property
+    def isWasBye(self) -> bool:
+        return self._is_was_bye
+    
+    @isWasBye.setter
+    def isWasBye(self, value) -> None:
+        self._is_was_bye = value
--- Tourney.py
+++ Tourney.py
@@ -1,17 +1,20 @@
 from dataclasses import dataclass , field
 from Player import Player
+import logging
+import traceback
 import random
 
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 @dataclass
 class Tourney:
 
     _players: list = field(default_factory=list)
     _round: int = field(default=0)
     _tables: list = field(default_factory=list)
-    _byeRule: bool = field(default=False)
-
-    def upRound(self) -> None:
-        self._round += 1
+    _dropped_players: list = field(default_factory=list)
+    _bye_rule: bool = field(default=False)
     
     @property
     def players(self) -> list:
@@ -39,25 +42,54 @@ class Tourney:
     
     @property
     def byeRule(self) -> bool:
-        return self._byeRule
+        return self._bye_rule
     
     @byeRule.setter
     def byeRule(self, value:bool) -> None:
-        self._byeRule = value
+        self._bye_rule = value
+
+    @property
+    def droppedPlayers(self) -> list:
+        return self._dropped_players
+    
+    @droppedPlayers.setter
+    def droppedPlayers(self, value:list) -> None:
+        self._dropped_players = value
+
+    def getPlayerByName (self, name:str) -> Player:
+        for player in self._players:
+            if player.name == name:
+                return player
 
     def addPlayers(self, players:str) -> None:
         new_players = players.split('\n')
         for player in new_players:
             player = Player(player)
             self._players.append(player)
+        logger.info(f"Players added: {new_players}")
 
     def removePlayer(self, name:str) -> bool:
         for player in self._players:
             if player.name == name:
-                self._player.remove(player) # Remove player from the list of players
+                self._player.remove(player)
                 del player
+                logger.info(f"Player removed: {name}")
                 return True
+        logger.info(f"Player removal failed: {name}")
         return False
+    
+    def dropPlayerByName(self, name:str) -> bool:
+        self.dropPlayer(self.getPlayerByName(name))
+    
+    def dropPlayer(self, player:Player) -> bool:
+        try:
+            player.isDrop = True
+            self._dropped_players.append(player)
+        except Exception:
+            traceback.print_exc()
+            return False
+        logger.info(f"Player dropped: {Player.name}")
+        return True
     
     def populateTables(self) -> bool:
         if len(self._players) > 4:
@@ -66,32 +98,84 @@ class Tourney:
         else:
             self._tables.appent(self._players)
         return False
+    
+    
+    def _scramble_round_0(self) -> None:
+        shuffled_players = [player for player in self.players if not player.isDrop()]
+        random.shuffle(shuffled_players)
+        self._tables = [shuffled_players[i:i + 4] for i in range(0, len(shuffled_players), 4)]
 
 
-    def scrambleTables(self) -> bool:
-        self.checkForDrops()
-        round = self.round
-        byePlayers = [player for player in self._players if player.isBye]
-        
-        if round == 0:
-            shuffled_players = self._players
-            random.shuffle(shuffled_players)
-            self._tables = [shuffled_players[i:i + 4] for i in range(0, len(shuffled_players), 4)]
+    def _scramble_rounds(self) -> None:
+        _bye_players = [player for player in self._players if player.isBye() or player.isWasBye()]
+        sorted_players = [player for player in sorted_players if not player.isBye() or player.isWasBye() or player.isDrop()]
+        for player in _bye_players:
+            sorted_players[-1].isBye = True
+            sorted_players.pop()
+            player.isWasBye = True
+            player.isBye = False
+        sorted_players.extend(_bye_players)
+        sorted_players = sorted(self._players, key=lambda player: (player.score, player.opponentMatch1, player.opponentMatch2, player.opponentMatch3))
+        self._tables = [sorted_players[i:i + 4] for i in range(0, len(sorted_players), 4)]
 
-        elif round > 0:
-            sorted_players = [player for player in sorted_players if not player.isBye]
-            for _ in byePlayers:
-                sorted_players.pop()
-            sorted_players.extend(byePlayers)
-            sorted_players = sorted(self._players, key=lambda player: (player.score, player.opponentMatch1, player.opponentMatch2, player.opponentMatch3))
-            self._tables = [sorted_players[i:i + 4] for i in range(0, len(sorted_players), 4)]
+    def _treat_byes(self) -> None:
+        last_table = self._tables[-1] if self._tables else []
+        if len(last_table) < 4:
+            for player in last_table:
+                player.isBye = True
+                player.score += 3
+            self._tables.pop()
 
-        else:
-            return False
-        
-        if self.byeRule:
-            last_table = self._tables[-1] if self._tables else []
-            if len(last_table) < 4:
-                for player in last_table:
-                    player.isBye = True
+    def _scramble_tables(self) -> bool:
+
+        if self._round == 0:
+            self._scramble_round_0()
+
+        if self._round > 0:
+            self._scramble_rounds()
+            
+
+        if self._bye_rule:
+            self._treat_byes()
         return True
+    
+    
+    def table_result(self, result_dict:dict[int, Player]) -> bool:
+        for table, winner in result_dict.items():
+            self._table_result(table, winner)
+
+    def _table_result(self, table:int, winner:Player = None) -> bool:
+        players = self._tables[table]
+        opponent_match2_list :list = [player.score for player in players]
+        opponent_match3_list :list = [player.opponentMatch2 for player in players]
+
+        for idx,player in enumerate(players):
+            player.opponentMatch2 += sum(opponent_match2_list[:idx] + opponent_match2_list[idx+1:])
+            player.opponentMatch3 += sum(opponent_match3_list[:idx] + opponent_match3_list[idx+1:])
+
+            if winner is not None:
+                if player == winner:
+                    player.score += 3
+                    player.roundsWon += 1
+            elif winner is None:
+                player.score += 1
+        return True
+    
+    def display_leaderboard(self) -> None:
+        sorted_players = sorted(self._players, key=lambda player: (player.score, player.opponentMatch1, player.opponentMatch2, player.opponentMatch3), reverse=True)
+        for player in sorted_players:
+            print(f"{player.name:<20} - {player.score:<2} - {player.roundsWon:<1} - {player.opponentMatch1:<3} - {player.opponentMatch2:<3} - {player.opponentMatch3:<3}")
+
+    def start_tourney(self) -> bool:
+        self.scramble_tables()
+        self._round += 1
+        return True
+    
+name = "Comedor de Casada123123"
+score = 3
+roundsWon = 1
+opponentMatch1 = 12
+opponentMatch2 = 32
+opponentMatch3 = 612
+
+print(f"{name:>15} Points: {score:<2} Rounds Won: {roundsWon:<1} CDD1: {opponentMatch2:<3} CDD2: {opponentMatch3:<3}")
